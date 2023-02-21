8ce02f1a51656c848d64202b0bcb35c6a48435aa
carterww <carterww@hotmail.com>
2023-02-21 15:21:34 -0600

    Start Mutual Fund

---
+++ Models/MutualFundClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace StockResearchPlatform.Models
+{
+	public class MutualFundClass : Stock
+	{
+		public MutualFundClass(string ticker) : base(ticker)
+		{
+		}
+		public string seriesID { get; set; }
+		public string classID { get; set; }
+	}
+}
+
