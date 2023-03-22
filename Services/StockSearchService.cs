28e594c43874328c9c4b08fed727641b7c0e738b
carterww <carterww@hotmail.com>
2023-03-21 23:09:50 -0500

    Fetch Stock Info

---
+++ Services/StockSearchService.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Extensions.Configuration;
+using StockResearchPlatform.Models;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace StockResearchPlatform.Services
+{
+    public class StockSearchService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly HttpService _httpService;
+        private readonly string? _apiKey;
+        private readonly string? _baseUrl;
+        
+        public StockSearchService(IConfiguration configuration, HttpService httpservice)
+        {
+            _configuration = configuration;
+            _httpService = httpservice;
+            _apiKey = _configuration.GetSection("AletheiaAPI")["Key"];
+            _baseUrl = _configuration.GetSection("AletheiaAPI")["BaseUrl"];
+        }
+
+        public async Task<StockInformation> fetchStockInformation(string ticker)
+        {
+            var client = _httpService.Client;
+            var reqUrl = _baseUrl + $"StockData?symbol={ticker.ToLower()}";
+
+            try
+            {
+                HttpResponseMessage? res = null;
+                using (var requestMessage = new HttpRequestMessage(HttpMethod.Get, reqUrl))
+                {
+                    requestMessage.Headers.Add("Key", _apiKey);
+                    requestMessage.Headers.Add("Accept-Version", "2");
+
+                    res = await client.SendAsync(requestMessage);
+                }
+
+                if (res != null)
+                {
+                    res.EnsureSuccessStatusCode();
+                    string resBody = await res.Content.ReadAsStringAsync();
+
+                    return JsonSerializer.Deserialize<StockInformation>(resBody);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return null;
+        }
+    }
+}
